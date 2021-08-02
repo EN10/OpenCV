6d4855ea364a901c2a3d750a232be6bd8d8e557a
Enio Fernandes <EN10@users.noreply.github.com>
2021-08-02 15:02:48 +0100

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -6,20 +6,19 @@ import os
 vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while(True):
-	
 	# Capture the video frame by frame
 	ret, frame = vid.read()
-
 	# Display the resulting frame
 	cv2.imshow('frame', frame)
+
 	# Save on pressing space
 	if cv2.waitKey(1) & 0xFF == ord(' '): 
 		# pwd
 		print(os.getcwd()) 
 		cv2.imwrite('c1.png',frame)
-		
-		# After the loop release the cap object
-		vid.release()
-		# Destroy all the windows
-		cv2.destroyAllWindows()
 		break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
